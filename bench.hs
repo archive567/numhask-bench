7b20e2ba82139deed4c49bea5ef8b6a17bfa5eea
Tony Day <tonyday567@gmail.com>
2019-12-21 12:32:06 +1000

    cleanup

--- bench.hs
+++ bench.hs
@@ -25,8 +25,6 @@ import Perf
 import Perf.Analysis
 import qualified Protolude as P
 import Readme.Lhs
-import qualified Statistics.Matrix as DLA
-import qualified Statistics.Matrix.Fast as DLAF
 
 -- | format a run median
 formatMedian :: (P.Integral a) => Text -> [[a]] -> [Text]
@@ -57,39 +55,33 @@ main :: IO ()
 main = do
   let !n = 100
   _ <- warmup 100
-  let ts :: [Text]
-      ts = ["NAF", "NAD", "NAH", "V", "H"]
-
   let ma x = ticks n (x `F.mmult`) x
   let mnad x = ticks n (x `D.mmult`) x
   let mnah x = ticks n (x `H.mmult`) x
   let mh x = ticks n (x HMatrix.<>) x
-  let md x = ticks n (DLAF.multiply x) x
   let !ma10 = [1 .. 100] :: F.Array '[10, 10] Double
   let !mnah10 = [1 .. 100] :: H.Array '[10, 10] Double
   let !mnad10 = D.fromFlatList [10, 10] [1 .. 100] :: D.Array Double
   let !mh10 = (10 HMatrix.>< 10) [1 :: HMatrix.R ..]
-  let !md10 = DLA.fromList 10 10 [1 .. (fromIntegral $ (10 :: Int) * 10)]
-  
   rma10 <- ma ma10
+  drma10 <- mnad (F.toDynamic ma10)
+  hrma10 <- mnah mnah10
   rmh10 <- mh mh10
-  rmd10 <- md md10
   rmnah10 <- mnah mnah10
   rmnad10 <- mnad mnad10
   putStrLn ("mmult 10x10" :: Text)
   putStrLn $ ("hmatrix " :: Text) <> formatF 2 (percentile 0.5 (fst rmh10))
-  putStrLn $ ("NAF " :: Text) <> formatF 2 (percentile 0.5 (fst rma10))
-  putStrLn $ ("Statistics.Matrix.Fast " :: Text) <> formatF 2 (percentile 0.5 (fst rmd10))
-  putStrLn $ ("H " :: Text) <> formatF 2 (percentile 0.5 (fst rmnah10))
-  putStrLn $ ("D " :: Text) <> formatF 2 (percentile 0.5 (fst rmnad10))
+  putStrLn $ ("Fixed " :: Text) <> formatF 2 (percentile 0.5 (fst rma10))
+  putStrLn $ ("HMatrix " :: Text) <> formatF 2 (percentile 0.5 (fst rmnah10))
+  putStrLn $ ("Dynamic " :: Text) <> formatF 2 (percentile 0.5 (fst rmnad10))
 
   r2 <- dot100 2
   r100 <- dot100 100
 
   void
     $ runOutput
-      ("other/results_.md", GitHubMarkdown)
-      ("results.md", GitHubMarkdown)
+      ("readme_.md", GitHubMarkdown)
+      ("readme.md", GitHubMarkdown)
     $ do
       output "values" $ Native $ (: []) $
         bool
@@ -100,23 +92,22 @@ main = do
         Native
           [ formatRunsMedian
               ["2", "100"]
-              (zip ts $ P.transpose [snd r2, snd r100])
+              (zip ["NumHask.Array.Fixed", "NumHask.Array.Dynamic", "vector"] $ P.transpose [snd r2, snd r100])
           ]
       output "mmult" $
         Native
           [ formatRunsMedian
               ["10"]
               [ ("NumHask.Array.Fixed", [fst rma10]),
-                ("Numeric.LinearAlgebra.R", [fst rmh10]),
-                ("Statistics.Matrix.Fast", [fst rmd10])
+                ("NumHask.Array.Dynamic", [fst drma10]),
+                ("NumHask.Array.HMatrix", [fst hrma10]),
+                ("Numeric.LinearAlgebra.R", [fst rmh10])
               ]
           ]
 
 tdotnaf n x = ticks n (F.dot sum (+) x) x
 tdotnad n x = ticks n (D.dot sum (+) x) x
-tdotnah n x = ticks n (H.mmult x) x
 tdotv n x = ticks n (sum . V.zipWith (*) x) x
-tdoth n x = ticks n (x HMatrix.<.>) x
 
 dot100 :: Int -> IO ([Double], [[Cycle]])
 dot100 n = do
---
+++ readme_.md
@@ -0,0 +1,30 @@
+Current Performance
+---
+
+dot
+
+```{.output .inner}
+```
+
+mmult
+
+```{.output .mmult}
+```
+
+numhask-bench
+=============
+
+[![Build
+Status](https://travis-ci.org/tonyday567/numhask-bench.png)](https://travis-ci.org/tonyday567/numhask-bench)
+
+recipe
+------
+
+    stack build --exec "$(stack path --local-install-root)/bin/numhask-bench"
+
+reference
+---------
+
+-   [perf](https://hackage.haskell.org/package/perf)
+-   [numhask-array](https://hackage.haskell.org/package/numhask-array)
+
