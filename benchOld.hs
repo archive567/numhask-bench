6a448a747d1103f8558a83e6b3a8c767762fe91d
Tony Day <tonyday567@gmail.com>
2022-04-11 17:53:44 +1000

    split into new and old

--- benchOld.hs
+++ benchOld.hs
@@ -21,13 +21,6 @@ import Perf
 import NumHask.Prelude as P hiding (option)
 import Options.Applicative
 import qualified Prelude
-import qualified Data.Map.Strict as Map
-import qualified Data.List as List
-import Data.Text (Text)
-import qualified Data.Text as Text
-import qualified Data.Text.IO as Text
-import Data.FormatN
-import GHC.TypeNats
 
 data MeasureType = MeasureTime | MeasureSpace | MeasureSpaceTime | MeasureAllocation deriving (Eq, Show)
 
@@ -75,101 +68,26 @@ opts :: ParserInfo Options
 opts = info (options <**> helper)
   (fullDesc <> progDesc "benchmark testing" <> header "A performance benchmark for numhask")
 
-rioOrg :: Maybe FilePath -> Maybe FilePath -> StatDType -> [Text] -> Map.Map [Text] [[Double]] -> IO ()
-rioOrg check record s labels m = do
-    case check of
-      Nothing -> printOrg (expt (Just 3) <$> m')
-      Just fp -> degradePrint defaultDegradeConfig fp m'
-    mapM_ (`writeResult` m') record
-    where
-      m' = Map.fromList $ mconcat $ (\(ks,xss) -> zipWith (\x l -> (ks <> [l], statD s x)) (List.transpose xss) labels) <$> Map.toList m
-
--- * org-mode formatting
-outercalate :: Text -> [Text] -> Text
-outercalate c ts = c <> Text.intercalate c ts <> c
-
-printOrgHeader :: Map.Map [Text] a -> [Text] -> IO ()
-printOrgHeader m ts = do
-  Text.putStrLn $ outercalate "|" ((("label" <>) . Text.pack . show <$> [1..labelCols]) <> ts)
-  Text.putStrLn $ outercalate "|" (replicate (labelCols+1) "---")
-  pure ()
-    where
-      labelCols = maximum $ length <$> Map.keys m
-
-printOrg :: Map.Map [Text] Text -> IO ()
-printOrg m = do
-  printOrgHeader m ["results"]
-  _ <- Map.traverseWithKey (\k a -> Text.putStrLn (outercalate "|" (k <> [a]))) m
-  pure ()
-
 main :: IO ()
 main = do
   o <- execParser opts
   let !n = optionRuns o
   let s = optionStatDType o
   let r = optionRunType o
-  let mt = optionMeasureType o
-  let fp = fromMaybe ("other/" <> show r <> ".csv") (optionsGolden o)
-  let record = bool Nothing (Just fp) (optionsRecord o)
-  let check = bool Nothing (Just fp) (optionsRecordCheck o)
-  let rio = rioOrg check record s
   _ <- warmup 100
 
   case r of
     RunMMult -> do
-      run_ rio mt n runMM
       runHMatrix s n
       runNHHMatrix s n
       runF s n
       runD s n
     RunDotSum -> do
-      run_ rio mt n runFDS_
       runFDS s n
     RunDotVector -> do
-      run_ rio mt n runDotV_
-      run_ rio mt n runDotF_
-      run_ rio mt n runDotD_
       runDotV s n
       runDotF s n
       runDotD s n
-
--- | unification of the different measurements to being a list of doubles.
-measureDs :: MeasureType -> Int -> Measure IO [[Double]]
-measureDs mt n =
-  case mt of
-    MeasureTime -> fmap ((:[]) . Prelude.fromIntegral) <$> times n
-    MeasureSpace -> toMeasureN n (ssToList <$> space False)
-    MeasureSpaceTime -> toMeasureN n ((\x y -> ssToList x <> [Prelude.fromIntegral y]) <$> space False <*> stepTime)
-    MeasureAllocation -> fmap ((:[]) . Prelude.fromIntegral) <$> toMeasureN n (allocation False)
-
--- | unification of the different measurements to being a list of doubles.
-measureLabels :: MeasureType -> [Text]
-measureLabels mt =
-  case mt of
-    MeasureTime -> ["time"]
-    MeasureSpace -> spaceLabels
-    MeasureSpaceTime -> spaceLabels <> ["time"]
-    MeasureAllocation -> ["allocation"]
-
-run_ :: ([Text] -> Map.Map [Text] [[Double]] -> IO b) -> MeasureType -> Int -> PerfT IO [[Double]] a -> IO b
-run_ rio mt n f = do
-      m <- execPerfT (measureDs mt n) f
-      rio (measureLabels mt) (Map.mapKeys (:[]) m)
-{-# Inline run_ #-}
-
-runMM :: (Semigroup t) => PerfT IO t ()
-runMM = do
-  _ <- fap "hmatrix" (\x -> x HMatrix.<> x) ((10 HMatrix.>< 10) [1 :: HMatrix.R ..])
-  _ <- fap "numhask-hmatrix" (\x -> x `H.mmult` x) ([1 .. 100] :: H.Array '[10, 10] Double)
-  _ <- fap "fixed" (\x -> x `F.mmult` x) ([1 .. 100] :: F.Array '[10, 10] Double)
-  _ <- fap "dynamic" (\x -> x `D.mmult` x) (D.fromFlatList [10, 10] [1 .. 100] :: D.Array Double)
-  pure ()
-{-# Inline runMM #-}
-
-runHMatrix_ :: (Semigroup t) => PerfT IO t (HMatrix.Matrix Double)
-runHMatrix_ = fap "hmatrix" (x HMatrix.<>) x
-  where
-    x = (10 HMatrix.>< 10) [1 :: HMatrix.R ..]
 
 runHMatrix :: StatDType -> Int -> IO ()
 runHMatrix s n = do
@@ -179,11 +97,6 @@ runHMatrix s n = do
   th10 <- ticksH h10
   putStrLn $ "hmatrix " <> show (statD s (Prelude.fromIntegral <$> fst th10))
 
-runNHHMatrix_ :: (Semigroup t) => PerfT IO t (H.Array '[10,10] Double)
-runNHHMatrix_ = fap "numhask-hmatrix" (x `H.mmult`) x
-  where
-    x = [1 .. 100] :: H.Array '[10, 10] Double
-
 runNHHMatrix :: StatDType -> Int -> IO ()
 runNHHMatrix s n = do
   -- NumHask.Array.HMatrix
@@ -191,11 +104,6 @@ runNHHMatrix s n = do
   let !h'10 = [1 .. 100] :: H.Array '[10, 10] Double
   th'10 <- ticksH' h'10
   putStrLn $ "numhask-hmatrix " <> show (statD s (Prelude.fromIntegral <$> fst th'10))
-
-runF_ :: (Semigroup t) => PerfT IO t (F.Array '[10,10] Double)
-runF_ = fap "fixed" (x `F.mmult`) x
-  where
-    x = [1 .. 100] :: F.Array '[10, 10] Double
 
 runF :: StatDType -> Int -> IO ()
 runF s n = do
@@ -205,11 +113,6 @@ runF s n = do
   tf10 <- ticksF f10
   putStrLn $ "Fixed " <> show (statD s (Prelude.fromIntegral <$> fst tf10))
 
-runFDS_ :: (Semigroup t) => PerfT IO t (F.Array '[10,10] Double)
-runFDS_ = fap "fixed" (F.dot sum (+) x) x
-  where
-    x = [1 .. 100] :: F.Array '[10, 10] Double
-
 runFDS :: StatDType -> Int -> IO ()
 runFDS s n = do
   -- NumHask.Array.Fixed.dot
@@ -218,22 +121,12 @@ runFDS s n = do
   tf10 <- ticksF f10
   putStrLn $ "Fixed-dotsum " <> show (statD s (Prelude.fromIntegral <$> fst tf10))
 
-runD_ :: (Semigroup t) => PerfT IO t (D.Array Double)
-runD_ = fap "dynamic" (x `D.mmult`) x
-  where
-    x = D.fromFlatList [10, 10] [1 .. 100] :: D.Array Double
-
 runD :: StatDType -> Int -> IO ()
 runD s n = do
   let !d10 = D.fromFlatList [10, 10] [1 .. 100] :: D.Array Double
   let ticksD x = ticks n (x `D.mmult`) x
   td10 <- ticksD d10
   putStrLn $ "Dynamic " <> show (statD s (Prelude.fromIntegral <$> fst td10))
-
-runDotV_ :: (Semigroup t) => PerfT IO t Double
-runDotV_ = fap "dynamic" (sum . V.zipWith (*) x) x
-  where
-    x = V.fromList [1 .. 100] :: V.Vector Double
 
 runDotV :: StatDType -> Int -> IO ()
 runDotV s n = do
@@ -243,11 +136,6 @@ runDotV s n = do
   tvd <- ticksDotv vv
   putStrLn $ "Vector-dot " <> show (statD s (Prelude.fromIntegral <$> fst tvd))
 
-runDotF_ :: (Semigroup t) => PerfT IO t (F.Array ('[]::[Nat]) Double)
-runDotF_ = fap "dynamic" (F.dot sum (+) x) x
-  where
-    x = P.fromList [1 .. 100] :: F.Array '[100] Double
-
 runDotF :: StatDType -> Int -> IO ()
 runDotF s n = do
   -- Fixed Dot
@@ -255,12 +143,6 @@ runDotF s n = do
   let ticksDotF x = ticks n (F.dot sum (+) x) x
   tfd <- ticksDotF vf
   putStrLn $ "Fixed-dot " <> show (statD s (Prelude.fromIntegral <$> fst tfd))
-
-runDotD_ :: (Semigroup t) => PerfT IO t (D.Array Double)
-runDotD_ = fap "dynamic" (D.dot sum (+) vd) vd2
-  where
-    vd = D.fromFlatList [100] [1 .. 100] :: D.Array Double
-    vd2 = D.fromFlatList [100] [101 .. 200] :: D.Array Double
 
 runDotD :: StatDType -> Int -> IO ()
 runDotD s n = do
